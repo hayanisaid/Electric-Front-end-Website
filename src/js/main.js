5f44bdf9c7a905328ec4d4cecce8f2814a7f6df5
hayani said <hayanisaid1995@gmail.com>
2018-01-18 22:07:57 +0000

    add and costumize about page

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,23 @@ $(document).ready(function(){
 
  $(window).scroll(function(){
  	var scroll=$(window).scrollTop();
- 	console.log(scroll);
+ 	//console.log(scroll);
  	if(scroll >0){
  		$("#sticky").removeClass("stick");
  	}else{
  		$("#sticky").addClass("stick");
  	}
+ })
+
+$('.button-collapse').sideNav();
+
+ /*scrooltop*/
+ $("#conScrol").click(function(){
+  $("body").animate({
+    scrollTop :$("#" + $(this).data("value")).offset().top
+
+  },1000)
+
  })
 
 })
